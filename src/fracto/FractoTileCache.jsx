92b6e492f42df1977e4180d2499cb1f547be8654
media-frameworks <mikehallvideo@gmail.com>
2025-02-07 19:00:02 -0500

    gzip in header

--- src/fracto/FractoTileCache.jsx
+++ src/fracto/FractoTileCache.jsx
@@ -15,6 +15,7 @@ const MIN_CACHE = 150
 const AXIOS_CONFIG = {
    responseType: 'blob',
    headers: {
+      'Accept-Encoding': 'gzip',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Expose-Headers': 'Access-Control-*',
       'Access-Control-Allow-Methods': 'GET,PUT,POST,PATCH,OPTIONS',
