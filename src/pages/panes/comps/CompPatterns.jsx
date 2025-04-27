8e9feea8326dd49aaeef59a1ddd49ff1ac911d05
media-frameworks <mikehallvideo@gmail.com>
2025-04-27 18:25:56 -0400

    meridians

--- src/pages/panes/comps/CompPatterns.jsx
+++ src/pages/panes/comps/CompPatterns.jsx
@@ -1,30 +1,17 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import {Scatter} from "react-chartjs-2";
 import {Chart as ChartJS, CategoryScale, BarController} from "chart.js/auto";
 
 import {CompPatternStyles as styles} from "styles/CompPatternStyles"
-import Complex from "common/math/Complex";
-import FractoFastCalc from "fracto/FractoFastCalc";
-import FractoUtil from "fracto/FractoUtil";
-import {render_pattern_block} from "fracto/styles/FractoStyles";
-import {
-   KEY_FOCAL_POINT,
-   KEY_HOVER_POINT
-} from "settings/AppSettings";
-import {KEY_COMPS_WIDTH_PX} from "settings/PaneSettings";
+import {KEY_PATTERN_TYPE} from "settings/CompSettings";
+import PatternsOrbital from "./patterns/PatternsOrbital";
+import PatternsMeridian from "./patterns/PatternsMeridian";
 
 ChartJS.register(CategoryScale, BarController)
 
-const GRID_CONFIG = {
-   color: function (context) {
-      return context.tick.value === 0 ? '#aaaaaa' : '#dddddd'
-   },
-   lineWidth: function (context) {
-      return context.tick.value === 0 ? 1.5 : 1
-   }
-};
+export const PATTERN_TYPE_ORBITALS = 'pattern_type_orbitals'
+export const PATTERN_TYPE_MERIDIANS = 'pattern_type_meridians'
 
 export class CompPatterns extends Component {
    static propTypes = {
@@ -32,137 +19,55 @@ export class CompPatterns extends Component {
       on_settings_changed: PropTypes.func.isRequired,
    }
 
-   click_point_chart = (set1, set2, in_cardioid = false) => {
-      const options = {
-         scales: {
-            x: {grid: GRID_CONFIG,},
-            y: {grid: GRID_CONFIG,}
-         },
-         animation: false,
-         maintainAspectRatio: false,
-      }
-      if (!in_cardioid) {
-         options.scales.x.min = -2
-         options.scales.x.max = 2
-         options.scales.y.min = -2
-         options.scales.y.max = 2
-      }
-      const cardinality = set1?.length - 1 || 0
-      const data_dataset = {
-         datasets: [
-            {
-               Id: 1,
-               label: `${cardinality || '?'}-point orbital`,
-               data: set1,
-               backgroundColor: FractoUtil.fracto_pattern_color(cardinality || 0),
-               showLine: true
-            },
-            {
-               Id: 2,
-               label: set2.length ? `P` : '(in cardioid)',
-               data: set2,
-               backgroundColor: 'black',
-               showLine: false
-            },
-         ]
-      }
-      return [
-         <Scatter
-            datasetIdKey='id1'
-            data={data_dataset} options={options}
+   set_pattern_type = (pattern_type) => {
+      const {on_settings_changed} = this.props
+      on_settings_changed({[KEY_PATTERN_TYPE]: pattern_type,})
+   }
+
+   render_pattern_type = () => {
+      const {page_settings} = this.props
+      const pattern_type = page_settings[KEY_PATTERN_TYPE] || PATTERN_TYPE_ORBITALS
+      const deselected_style = {color: '#aaaaaa', fontWeight: 400}
+      return <styles.CenteredBlock>
+         <input
+            type={"radio"}
+            checked={pattern_type === PATTERN_TYPE_ORBITALS}
+            onClick={() => this.set_pattern_type(PATTERN_TYPE_ORBITALS)}
          />
-      ]
-   }
-
-   get_fracto_values = (set1, set2) => {
-      const {page_settings} = this.props
-      let click_point = page_settings[KEY_HOVER_POINT]
-      if (!click_point) {
-         click_point = page_settings[KEY_FOCAL_POINT]
-      }
-      return FractoFastCalc.calc(click_point.x, click_point.y)
-   }
-
-   escape_points_chart = (click_point) => {
-      const P_x = click_point.x
-      const P_y = click_point.y
-      let Q_x_squared = 0
-      let Q_y_squared = 0
-      let Q_x = 0
-      let Q_y = 0
-      const all_points = [{x: 0, y: 0}]
-      for (let iteration = 0; iteration < 10000; iteration++) {
-         Q_y = 2 * Q_x * Q_y + P_y;
-         Q_x = Q_x_squared - Q_y_squared + P_x;
-         Q_x_squared = Q_x * Q_x
-         Q_y_squared = Q_y * Q_y
-         const sum_squares = Q_x_squared + Q_y_squared
-         if (sum_squares > 4) {
-            break;
-         }
-         all_points.push({x: Q_x, y: Q_y})
-      }
-      return this.click_point_chart(all_points, [], false)
-   }
-
-   click_point_data = () => {
-      const {page_settings} = this.props
-      let click_point = page_settings[KEY_HOVER_POINT]
-      if (!click_point) {
-         click_point = page_settings[KEY_FOCAL_POINT]
-      }
-      const fracto_values = this.get_fracto_values()
-      const in_cardioid = FractoFastCalc.point_in_main_cardioid(click_point.x, click_point.y)
-      if (fracto_values.pattern) {
-         return this.click_point_chart(
-            fracto_values.orbital_points,
-            in_cardioid ? [] : [{x: click_point.x, y: click_point.y}],
-            in_cardioid
-         )
-      }
-      return this.escape_points_chart(click_point)
-   }
-
-   render_info = () => {
-      const {page_settings} = this.props
-      const fracto_values = this.get_fracto_values()
-      const current_pattern = (fracto_values.orbital_points?.length || 1) - 1
-      if (current_pattern) {
-         let click_point = page_settings[KEY_HOVER_POINT]
-         if (!click_point) {
-            click_point = page_settings[KEY_FOCAL_POINT]
-         }
-         const c = new Complex(click_point.x, click_point.y)
-         const neg_c = c.scale(-1)
-         let z = new Complex(0, 0)
-         for (let i = 0; i < current_pattern + 10; i++) {
-            z = z.add(neg_c)
-            console.log('z', z.toString())
-            z = z.sqrt()
-         }
-         console.log('fracto_values.orbital_points', fracto_values.orbital_points)
-         return <styles.InfoBlockWrapper>
-            <styles.PatternBlockWrapper>
-               {render_pattern_block(current_pattern)}
-            </styles.PatternBlockWrapper>
-            {/*{render_coordinates(product.re, product.im)}*/}
-         </styles.InfoBlockWrapper>
-      }
-      return <styles.InfoPrompt>
-         {'Hover or click to show orbital points'}
-      </styles.InfoPrompt>
+         <styles.PatternTypePrompt
+            style={pattern_type === PATTERN_TYPE_MERIDIANS ? deselected_style : {}}
+            onClick={() => this.set_pattern_type(PATTERN_TYPE_ORBITALS)}
+         >{'orbital'}
+         </styles.PatternTypePrompt>
+         <styles.Spacer/>
+         <input
+            type={"radio"}
+            checked={pattern_type === PATTERN_TYPE_MERIDIANS}
+            onClick={() => this.set_pattern_type(PATTERN_TYPE_MERIDIANS)}
+         />
+         <styles.PatternTypePrompt
+            style={pattern_type === PATTERN_TYPE_ORBITALS ? deselected_style : {}}
+            onClick={() => this.set_pattern_type(PATTERN_TYPE_MERIDIANS)}
+         >
+            {'meridian'}
+         </styles.PatternTypePrompt>
+      </styles.CenteredBlock>
    }
 
    render() {
-      const {page_settings} = this.props
-      const wrapper_dimension = Math.round(page_settings[KEY_COMPS_WIDTH_PX] * 0.9)
-      const wrapper_style = {
-         width: `${wrapper_dimension}px`,
-         height: `${wrapper_dimension}px`
-      }
-      return <styles.ContentWrapper style={wrapper_style}>
-         {this.click_point_data()}
-         {this.render_info()}
+      const {page_settings, on_settings_changed} = this.props
+      const pattern_content = page_settings[KEY_PATTERN_TYPE] === PATTERN_TYPE_ORBITALS
+         ? <PatternsOrbital
+            page_settings={page_settings}
+            on_settings_changed={on_settings_changed}
+         />
+         : <PatternsMeridian
+            page_settings={page_settings}
+            on_settings_changed={on_settings_changed}
+         />
+      return <styles.ContentWrapper>
+         {this.render_pattern_type()}
+         {pattern_content}
       </styles.ContentWrapper>
    }
 }
--- src/styles/CompPatternStyles.jsx
+++ src/styles/CompPatternStyles.jsx
@@ -23,6 +23,23 @@ export class CompPatternStyles {
        font-weight: 400;
        color: #aaaaaa;
    `
+   static CenteredBlock = styled(CoolStyles.Block)`
+       ${CoolStyles.align_left}
+       margin-top: 0.5rem;
+       border-bottom: 0.1rem solid #aaaaaa;
+   `
+   static Spacer = styled(CoolStyles.InlineBlock)`
+       margin-left: 1rem;
+   `
+   static PatternTypePrompt = styled(CoolStyles.InlineBlock)`
+       ${CoolStyles.pointer}
+       ${CoolStyles.bold}
+       ${CoolStyles.uppercase}
+       ${CoolStyles.deep_blue_text}
+       font-size: 1.125rem;
+       margin-left: 0.25rem;
+       margin-bottom: 0.25rem;
+   `
 }
 
 export default CompPatternStyles
