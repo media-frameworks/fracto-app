90d3dfd7edb53ba1f4c8c005f50c2fec5406b346
media-frameworks <mikehallvideo@gmail.com>
2025-05-11 09:00:15 -0400

    meridians

--- src/pages/panes/comps/patterns/PatternsOrbital.jsx
+++ src/pages/panes/comps/patterns/PatternsOrbital.jsx
@@ -14,6 +14,11 @@ import {
    KEY_HOVER_POINT,
 } from "settings/AppSettings";
 import {KEY_COMPS_WIDTH_PX} from "settings/PaneSettings";
+import CoolTable, {
+   CELL_ALIGN_CENTER,
+   CELL_ALIGN_LEFT,
+   CELL_TYPE_NUMBER,
+} from "common/ui/CoolTable";
 
 ChartJS.register(CategoryScale, BarController)
 
@@ -26,13 +31,49 @@ const GRID_CONFIG = {
    }
 };
 
+const ORBITAL_POINTS_COLUMNS = [
+   {
+      id: "index",
+      label: "#",
+      type: CELL_TYPE_NUMBER,
+      width_px: 50,
+      align: CELL_ALIGN_CENTER
+   },
+   {
+      id: "r",
+      label: "r",
+      type: CELL_TYPE_NUMBER,
+      width_px: 200,
+      align: CELL_ALIGN_LEFT
+   },
+   {
+      id: "i",
+      label: "i",
+      type: CELL_TYPE_NUMBER,
+      width_px: 200,
+      align: CELL_ALIGN_LEFT
+   },
+   {
+      id: "theta",
+      label: "theta",
+      type: CELL_TYPE_NUMBER,
+      width_px: 200,
+      align: CELL_ALIGN_LEFT
+   },
+]
+
 export class PatternsOrbital extends Component {
    static propTypes = {
       page_settings: PropTypes.object.isRequired,
       on_settings_changed: PropTypes.func.isRequired,
    }
 
-   click_point_chart = (set1, set2, in_cardioid = false) => {
+   state = {
+      orbital_points: [],
+      Q_core_neg: null,
+   }
+
+   click_point_chart = (set1, set2, in_cardioid = false, escaper = false) => {
       const options = {
          scales: {
             x: {grid: GRID_CONFIG,},
@@ -48,18 +89,21 @@ export class PatternsOrbital extends Component {
          options.scales.y.max = 2
       }
       const cardinality = set1?.length - 1 || 0
+      const set1_label = escaper
+         ? `escapes in ${cardinality || '?'} steps`
+         : `${cardinality || '?'} point${in_cardioid ? 's in cardiod' : ' orbital'}`
       const data_dataset = {
          datasets: [
             {
                Id: 1,
-               label: `${cardinality || '?'}-point orbital`,
+               label: set1_label,
                data: set1,
                backgroundColor: FractoUtil.fracto_pattern_color(cardinality || 0),
                showLine: true
             },
             {
                Id: 2,
-               label: set2.length ? `P` : '(in cardioid)',
+               label: in_cardioid ? 'Q' : 'Q',
                data: set2,
                backgroundColor: 'black',
                showLine: false
@@ -83,6 +127,15 @@ export class PatternsOrbital extends Component {
       return FractoFastCalc.calc(click_point.x, click_point.y)
    }
 
+   calculate_cardioid_Q = (x, y, scalar = 1) => {
+      const P = new Complex(x, y)
+      const negative_four_P = P.scale(-4.0)
+      const under_radocal = negative_four_P.offset(1, 0)
+      const radical = under_radocal.sqrt().scale(scalar)
+      const result = radical.offset(1.0, 0).scale(0.5)
+      return {x: result.re, y: result.im}
+   }
+
    escape_points_chart = (click_point) => {
       const P_x = click_point.x
       const P_y = click_point.y
@@ -90,7 +143,7 @@ export class PatternsOrbital extends Component {
       let Q_y_squared = 0
       let Q_x = 0
       let Q_y = 0
-      const all_points = [{x: 0, y: 0}]
+      const escape_points = [{x: 0, y: 0}]
       for (let iteration = 0; iteration < 10000; iteration++) {
          Q_y = 2 * Q_x * Q_y + P_y;
          Q_x = Q_x_squared - Q_y_squared + P_x;
@@ -100,9 +153,14 @@ export class PatternsOrbital extends Component {
          if (sum_squares > 4) {
             break;
          }
-         all_points.push({x: Q_x, y: Q_y})
+         escape_points.push({x: Q_x, y: Q_y})
       }
-      return this.click_point_chart(all_points, [], false)
+      const Q_core_neg = this.calculate_cardioid_Q(click_point.x, click_point.y, -1)
+      setTimeout(() => {
+         this.setState({orbital_points: escape_points, Q_core_neg})
+      }, 1000)
+
+      return this.click_point_chart(escape_points, [Q_core_neg], false, true)
    }
 
    click_point_data = () => {
@@ -113,11 +171,15 @@ export class PatternsOrbital extends Component {
       }
       const fracto_values = this.get_fracto_values()
       const in_cardioid = FractoFastCalc.point_in_main_cardioid(click_point.x, click_point.y)
+      const Q_core_neg = this.calculate_cardioid_Q(click_point.x, click_point.y, -1)
       if (fracto_values.pattern) {
+         setTimeout(() => {
+            this.setState({orbital_points: fracto_values.orbital_points, Q_core_neg})
+         }, 1000)
          return this.click_point_chart(
             fracto_values.orbital_points,
-            in_cardioid ? [] : [{x: click_point.x, y: click_point.y}],
-            in_cardioid
+            [Q_core_neg],
+            in_cardioid, false
          )
       }
       return this.escape_points_chart(click_point)
@@ -153,6 +215,37 @@ export class PatternsOrbital extends Component {
       </styles.InfoPrompt>
    }
 
+   click_point_table = () => {
+      const {orbital_points, Q_core_neg} = this.state
+      if (!orbital_points) {
+         return []
+      }
+      let prev_x = 0
+      let prev_y = 0
+      const table_data = orbital_points.map((point, i) => {
+         let m1 = 0
+         let m2 = 0
+         let theta = 0
+         if (i > 0) {
+            m1 = (point.y - Q_core_neg.y) / (point.x - Q_core_neg.x)
+            m2 = (prev_y - Q_core_neg.y) / (prev_x - Q_core_neg.x)
+            theta = Math.atan((m1 - m2)/(1 + m1 * m2))
+         }
+         prev_x = point.x
+         prev_y = point.y
+         return {
+            index: i + 1,
+            r: point.x,
+            i: point.y,
+            theta
+         }
+      })
+      return <CoolTable
+         data={table_data}
+         columns={ORBITAL_POINTS_COLUMNS}
+      />
+   }
+
    render() {
       const {page_settings} = this.props
       let wrapper_dimension = page_settings[KEY_COMPS_WIDTH_PX]
@@ -165,6 +258,7 @@ export class PatternsOrbital extends Component {
       }
       return <styles.ContentWrapper style={wrapper_style}>
          {this.click_point_data()}
+         {this.click_point_table()}
          {this.render_info()}
       </styles.ContentWrapper>
    }
