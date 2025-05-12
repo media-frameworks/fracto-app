c41f3a36451048ed9b45cb0cabdcde02cf6830f8
media-frameworks <mikehallvideo@gmail.com>
2025-05-12 19:42:30 -0400

    better color wheel

--- src/pages/panes/comps/orbitals/OrbitalsColorWheel.jsx
+++ src/pages/panes/comps/orbitals/OrbitalsColorWheel.jsx
@@ -32,14 +32,17 @@ export class OrbitalsColorWheel extends Component {
    componentDidUpdate(prevProps, prevState, snapshot) {
       const {most_recent} = this.state
       const {page_settings} = this.props
-      const {scope, focal_point} = page_settings
+      const {scope, focal_point, canvas_buffer} = page_settings
       const mr_scope = most_recent.scope
       const mr_focal_point = most_recent.focal_point
       const scope_changed = mr_scope !== scope
       const focal_point_x_changed = mr_focal_point.x !== focal_point?.x
       const focal_point_y_changed = mr_focal_point.y !== focal_point?.y
+      const canvas_buffer_changed = canvas_buffer && !prevProps.canvas_buffer
       if (scope_changed || focal_point_x_changed || focal_point_y_changed) {
          this.setState({most_recent: {scope, focal_point}})
+         this.fill_pattern_bins()
+      } else if (canvas_buffer_changed) {
          this.fill_pattern_bins()
       }
    }
@@ -53,7 +56,9 @@ export class OrbitalsColorWheel extends Component {
       }
       const dimension_px = page_settings[KEY_COMPS_HEIGHT_PX] * 0.40
       const orbital_bins = collect_orbitals(canvas_buffer)
-      this.setState({ dimension_px})
+      setTimeout(() => {
+         this.setState({dimension_px})
+      }, 100)
       const radius = dimension_px / 2 - 10
       if (radius > 0) {
          color_wheel(canvas_ref, radius, 8, 0, orbital_bins)
