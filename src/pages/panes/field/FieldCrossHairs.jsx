567fef334df82814418c460e8f4505ac55ccb773
media-frameworks <mikehallvideo@gmail.com>
2025-06-07 17:02:20 -0400

    scrolling during animation

---
+++ src/pages/panes/field/FieldCrossHairs.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {PaneFieldStyles as styles} from 'styles/PaneFieldStyles'
+import {HEADER_HEIGHT_PX} from "styles/PageAppStyles";
+import {faCaretDown, faCaretUp} from "@fortawesome/free-solid-svg-icons";
+import {KEY_DISABLED, KEY_SCOPE} from "settings/AppSettings";
+
+export class FieldCrossHairs extends Component {
+
+   render() {
+      return 'FieldContextMenu'
+   }
+}
+
+export default FieldCrossHairs
