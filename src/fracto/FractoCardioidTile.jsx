16c150a84d18efbd0cfcddb505bb06a5491fc0f9
media-frameworks <mikehallvideo@gmail.com>
2025-07-23 18:49:51 -0400

    FractoTileDetail

--- src/fracto/FractoCardioidTile.jsx
+++ src/fracto/FractoCardioidTile.jsx
@@ -1,3 +1,5 @@
+import FractoTileCache from "./FractoTileCache";
+
 const TILE_KEY_P_RE = 'p_re'
 const TILE_KEY_P_IM = 'p_im'
 const TILE_KEY_CENTER_RE = 'center_re'
@@ -11,6 +13,10 @@ const TILE_KEY_CARDINALITY = 'cardinality'
 const TILE_KEY_CYCLES = 'cycles'
 const TILE_KEY_MAGNITUDE = 'magnitude'
 const TILE_KEY_VARIANCE = 'variance'
+const TILE_KEY_IN_CARDIOID = 'in_cardioid'
+const TILE_KEY_ITERATIONS = 'iterations'
+const TILE_KEY_ROW_INDEX = 'row_index'
+const TILE_KEY_COL_INDEX = 'col_index'
 
 const CSV_COLUMNS = {
    TILE_KEY_P_RE,
@@ -26,12 +32,17 @@ const CSV_COLUMNS = {
    TILE_KEY_CYCLES,
    TILE_KEY_MAGNITUDE,
    TILE_KEY_VARIANCE,
+   TILE_KEY_IN_CARDIOID,
+   TILE_KEY_ITERATIONS,
+   TILE_KEY_ROW_INDEX,
+   TILE_KEY_COL_INDEX,
 }
 
-export class FractoCardioidTile {
+export class FractoTileDetail {
 
-   static generate = () => {
-
+   static generate = (short_code) => {
+      const tile = FractoTileCache.get_tile(short_code)
+      console.log(tile)
    }
 }
 
